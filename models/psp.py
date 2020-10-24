caa91e79b8f3c6dbffaffa971b854f58b53a13ba
Levin Dabhi <levindabhi17@gmail.com>
2020-10-24 15:58:04 +0530

    Set default value of resize False

--- models/psp.py
+++ models/psp.py
@@ -63,7 +63,7 @@ class pSp(nn.Module):
 			else:
 				self.__load_latent_avg(ckpt, repeat=18)
 
-	def forward(self, x, resize=True, latent_mask=None, input_code=False, randomize_noise=True,
+	def forward(self, x, resize=False, latent_mask=None, input_code=False, randomize_noise=True,
 	            inject_latent=None, return_latents=False, alpha=None):
 		if input_code:
 			codes = x
